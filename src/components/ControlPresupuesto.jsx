fb8651279744384b17cd4ac6a9db761d35cc4481
Valentín Alessandrini Zapata <79989652+valentinaless07@users.noreply.github.com>
2022-02-07 10:36:12 -0300

    progressBar

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,16 +1,20 @@
 import {useEffect, useState} from 'react';
+import {CircularProgressbar} from "react-circular-progressbar"
+import "react-circular-progressbar/dist/styles.css"
 
 export const ControlPresupuesto = ({presupuesto, gastos}) => {
 
   const [disponible, setDisponible] = useState(0)
   const [gastado, setGastado] = useState(0)
+  const [porcentaje, setPorcentaje] = useState(0);  
 
-
+  
   useEffect(() => {
     const totalGastado = gastos.reduce((total, gasto) => gasto.cantidad + total, 0)
-
+    const totalDisponible = presupuesto - totalGastado
+    const nuevoPorcentaje = ((presupuesto - totalDisponible) / presupuesto) * 100
     setGastado(totalGastado)
-    setDisponible(presupuesto - totalGastado)
+    setDisponible(totalDisponible)
     
   }, [gastos])
 
@@ -28,7 +32,9 @@ export const ControlPresupuesto = ({presupuesto, gastos}) => {
   return (
   <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
       <div>
-        <p>Grafica aquí</p>
+        <CircularProgressbar
+        value={porcentaje}
+        />
       </div>
 
       <div className='contenido-presupuesto'>
